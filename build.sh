221f8b6237bfcbbdfa9ec85bcb413c40a52e3a69
Hugo Klepsch <hugo.klepsch@gmail.com>
2025-11-22 09:01:09 -0500

    Add missing line ending to build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 # Build the hugo site. Invoke hugo using a docker container to avoid version conflicts.
 docker run --rm -it -v $(pwd)/hugo-site:/src -w /src --user $(id -u) hugomods/hugo:std-base-non-root-0.149.1 hugo
+
